6d5bf0fcc211e7a1675d2b1316d42777095f27b0
lbpinheiro <pinheiro.lucas@gmail.com>
2023-04-10 23:21:55 -0300

    sistema de duplas

--- criadorplacar.py
+++ criadorplacar.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import io
 import os
 import re
@@ -15,9 +16,8 @@ load_dotenv()
 # TODO
 # import inotify.adapters
 
-
 # constants
-VERSION = "0.3"
+VERSION = "0.5"
 LOG_FOLDER = "log"
 LOG_FILE = "criadorplacarbot.log"
 # LOG_FILE_MAX_SIZE = 10000000 # 10 mb # TODO with inotify
@@ -26,8 +26,10 @@ MAX_MESSAGES_PER_SECOND = 4
 FONT_FILE_NAME = "LiberationSans-Regular.ttf"
 NAME_MIN = 3
 NAME_MAX = 80
-OFFSET = 8  # depende da fonte
+OFFSET = int(os.environ.get("OFFSET"))  # depende da fonte
 
+
+TIPO_INVALIDO = "O tipo de jogo deve ser 'simples' ou 'duplas'"
 JOGADOR_INVALIDO = (f"O nome de um jogador deve possuir"
                     f"entre {NAME_MIN} e {NAME_MAX} caracteres")
 WELCOME = (f"Seja bem-vindo ao CriadorPlacarBot! Este bot tem como "
@@ -37,7 +39,8 @@ WELCOME = (f"Seja bem-vindo ao CriadorPlacarBot! Este bot tem como "
            f"facilitar a geração da imagem.\n\n"
            f"Por enquanto, não há verificação de dados, como locais ou nomes "
            f"de jogadores. É possível usar letras maiúsculas ou minúsculas em "
-           f"todos os campos.\n\n"
+           f"todos os campos (e em alguns casos, até botões de respostas "
+           f"rápidas).\n\n"
            f"Para iniciar, use o comando /placar. Para interromper o processo "
            f"atual, use o comando /cancelar.\n\n"
            f"Para ver esta mensagem de ajuda novamente, basta usar o comando "
@@ -54,7 +57,7 @@ class FieldDimensions(NamedTuple):
     y: int
 
 
-TENISTA_1 = FieldDimensions(440, 69, 616, 190)
+""" TENISTA_1 = FieldDimensions(440, 69, 616, 190)
 TENISTA_2 = FieldDimensions(440, 69, 616, 315)
 CAT_1 = FieldDimensions(86, 77, 951, 192)
 CAT_2 = FieldDimensions(86, 77, 951, 315)
@@ -62,8 +65,33 @@ PLACAR_1 = FieldDimensions(245, 130, 594, 603)
 PLACAR_2 = FieldDimensions(245, 130, 594, 837)
 TIEBREAK_1 = FieldDimensions(140, 130, 888, 603)
 TIEBREAK_2 = FieldDimensions(140, 130, 888, 837)
-LOCAL = FieldDimensions(530, 68, 674, 994)
+LOCAL = FieldDimensions(530, 68, 674, 994) """
 
+simplesDimensions = {
+    "TENISTA_1": FieldDimensions(440, 69, 616, 190),
+    "TENISTA_2": FieldDimensions(440, 69, 616, 315),
+    "CAT_1": FieldDimensions(86, 77, 951, 192),
+    "CAT_2": FieldDimensions(86, 77, 951, 315),
+    "PLACAR_1": FieldDimensions(245, 130, 594, 603),
+    "PLACAR_2": FieldDimensions(245, 130, 594, 837),
+    "TIEBREAK_1": FieldDimensions(140, 130, 888, 603),
+    "TIEBREAK_2": FieldDimensions(140, 130, 888, 837),
+    "LOCAL": FieldDimensions(530, 68, 674, 994)
+}
+
+duplasDimensions = {
+    "DUPLA_1": FieldDimensions(384, 47, 357, 123),
+    "DUPLA_2": FieldDimensions(384, 47, 357, 205),
+    "CAT_1": FieldDimensions(57, 50, 604, 126),
+    "CAT_2": FieldDimensions(57, 50, 685, 126),
+    "CAT_3": FieldDimensions(57, 50, 604, 200),
+    "CAT_4": FieldDimensions(57, 50, 685, 200),
+    "PLACAR_1": FieldDimensions(98, 94, 388, 395),
+    "PLACAR_2": FieldDimensions(98, 94, 391, 548),
+    "TIEBREAK_1": FieldDimensions(98, 94, 582, 395),
+    "TIEBREAK_2": FieldDimensions(98, 94, 584, 548),
+    "LOCAL": FieldDimensions(355, 50, 441, 650)  # 196
+}
 
 # set up logging
 if not os.path.exists(LOG_FOLDER):
@@ -87,6 +115,11 @@ user_info = {}
 state = {}
 
 
+def validate_tipo(text):
+    s = text.upper()
+    return s in {"SIMPLES", "DUPLAS"}
+
+
 def validate_name(text):
     return NAME_MIN <= len(text) <= NAME_MAX
 
@@ -97,7 +130,7 @@ def validate_local(text):
 
 def validate_cat(text):
     s = text.upper()
-    return s == "C" or s == "B" or s == "A" or s == "S" or s == "SS"
+    return s in {"C", "B", "A", "S", "SS"}
 
 
 def validate_score(text):
@@ -193,24 +226,24 @@ def create_image(chat_id):
     draw = ImageDraw.Draw(img)
 
     text = user_info[chat_id]["player1"].title()
-    draw_text(TENISTA_1, text, draw)
+    draw_text(simplesDimensions["TENISTA_1"], text, draw)
 
     text = user_info[chat_id]["player2"].title()
-    draw_text(TENISTA_2, text, draw)
+    draw_text(simplesDimensions["TENISTA_2"], text, draw)
 
     text = user_info[chat_id]["cat1"].upper()
-    draw_text(CAT_1, text, draw)
+    draw_text(simplesDimensions["CAT_1"], text, draw)
 
     text = user_info[chat_id]["cat2"].upper()
-    draw_text(CAT_2, text, draw)
+    draw_text(simplesDimensions["CAT_2"], text, draw)
 
     score = user_info[chat_id]["score"]
 
     text = score[:1]
-    draw_text(PLACAR_1, text, draw)
+    draw_text(simplesDimensions["PLACAR_1"], text, draw)
 
     text = score[1:2]
-    draw_text(PLACAR_2, text, draw)
+    draw_text(simplesDimensions["PLACAR_2"], text, draw)
 
     if len(score) > 2:
         match = re.search(r"\((\d+)\)", score)
@@ -222,15 +255,15 @@ def create_image(chat_id):
 
         # tiebreak 1
         text = str(tie1)
-        draw_text(TIEBREAK_1, text, draw)
+        draw_text(simplesDimensions["TIEBREAK_1"], text, draw)
 
         # tiebreak 2
         text = str(tie2)
-        draw_text(TIEBREAK_2, text, draw)
+        draw_text(simplesDimensions["TIEBREAK_2"], text, draw)
 
     # local
     text = user_info[chat_id]["local"].upper()
-    draw_text(LOCAL, text, draw)
+    draw_text(simplesDimensions["LOCAL"], text, draw)
 
     # Save the image
     img_byte_arr = io.BytesIO()
@@ -244,30 +277,134 @@ def create_image(chat_id):
     )
 
 
+def create_image_duplas(chat_id):
+    img = Image.open("ranking-duplas.jpg")
+    draw = ImageDraw.Draw(img)
+
+    text = user_info[chat_id]["player1"].title() + \
+        "/" + user_info[chat_id]["player2"].title()
+
+    draw_text(duplasDimensions["DUPLA_1"], text, draw)
+
+    text = user_info[chat_id]["player3"].title() + \
+        "/" + user_info[chat_id]["player4"].title()
+
+    draw_text(duplasDimensions["DUPLA_2"], text, draw)
+
+    text = user_info[chat_id]["cat1"].upper()
+    draw_text(duplasDimensions["CAT_1"], text, draw)
+
+    text = user_info[chat_id]["cat2"].upper()
+    draw_text(duplasDimensions["CAT_2"], text, draw)
+
+    text = user_info[chat_id]["cat3"].upper()
+    draw_text(duplasDimensions["CAT_3"], text, draw)
+
+    text = user_info[chat_id]["cat4"].upper()
+    draw_text(duplasDimensions["CAT_4"], text, draw)
+
+    score = user_info[chat_id]["score"]
+
+    text = score[:1]
+    draw_text(duplasDimensions["PLACAR_1"], text, draw)
+
+    text = score[1:2]
+    draw_text(duplasDimensions["PLACAR_2"], text, draw)
+
+    if len(score) > 2:
+        match = re.search(r"\((\d+)\)", score)
+        tie2 = int(match.group(1))
+        tie1 = 7
+
+        if tie2 > 5:
+            tie1 = tie2 + 2
+
+        # tiebreak 1
+        text = str(tie1)
+        draw_text(duplasDimensions["TIEBREAK_1"], text, draw)
+
+        # tiebreak 2
+        text = str(tie2)
+        draw_text(duplasDimensions["TIEBREAK_2"], text, draw)
+
+    # local
+    text = user_info[chat_id]["local"].upper()
+    draw_text(duplasDimensions["LOCAL"], text, draw)
+
+    # Save the image
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    bot.send_photo(chat_id=chat_id, photo=img_byte_arr)
+
+    logger.info(
+        (f"photo sent, chat_id={chat_id}, "
+         f"user_info={user_info[chat_id]}")
+    )
+
+
+""" markup = telebot.service_utils.quick_markup(
+    {'text': 'Press me', 'callback_data': 'press'},
+    {'text': 'Press me too', 'callback_data': 'press_too'}
+)
+
+inlineMarkup = types.InlineKeyboardMarkup()
+inlineMarkup.rows = 2
+catInlineItem1 = types.InlineKeyboardButton('Simples')
+catInlineItem2 = types.InlineKeyboardButton('Duplas')
+inlineMarkup.add(catInlineItem1, catInlineItem2) """
+
+catMarkup = types.ReplyKeyboardMarkup(row_width=2)
+catItem1 = types.KeyboardButton('C')
+catItem2 = types.KeyboardButton('B')
+catItem3 = types.KeyboardButton('A')
+catItem4 = types.KeyboardButton('S')
+catItem5 = types.KeyboardButton('SS')
+catMarkup.add(catItem1, catItem2, catItem3, catItem4, catItem5)
+
+tipoMarkup = types.ReplyKeyboardMarkup(row_width=1)
+tipoItem1 = types.KeyboardButton('Simples')
+tipoItem2 = types.KeyboardButton('Duplas')
+tipoMarkup.add(tipoItem1, tipoItem2)
+
+defaultMarkup = types.ReplyKeyboardRemove(selective=False)
+
+initialHandler = [
+    "Selecione a categoria do jogo (simples ou duplas)",
+    TIPO_INVALIDO,
+    validate_tipo,
+    "tipo",
+    tipoMarkup
+]
+
 stateHandler = [
     (
         "Insira o nome do jogador vencedor",
         JOGADOR_INVALIDO,
         validate_name,
-        "player1"
+        "player1",
+        defaultMarkup
     ),
     (
         "Insira a categoria do jogador vencedor (C, B, A, S ou SS)",
         "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
         validate_cat,
-        "cat1"
+        "cat1",
+        catMarkup
     ),
     (
         "Insira o nome do jogador derrotado",
         JOGADOR_INVALIDO,
         validate_name,
-        "player2"
+        "player2",
+        defaultMarkup
     ),
     (
         "Insira a categoria do jogador derrotado (C, B, A, S ou SS)",
         "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
         validate_cat,
-        "cat2"
+        "cat2",
+        catMarkup
     ),
     (
         "Insira o placar do jogo, sempre em referência ao vencedor e apenas "
@@ -275,13 +412,95 @@ stateHandler = [
         "pontos do perdedor entre parênteses, exemplos: 76(4), 76(9)",
         "Placar inválido",
         validate_score,
-        "score"
+        "score",
+        defaultMarkup
     ),
     (
         "Insira o nome do local onde ocorreu o jogo",
         "Local inválido",
         validate_local,
-        "local"
+        "local",
+        defaultMarkup
+    )
+]
+
+stateHandlerDuplas = [
+    (
+        "Insira o nome do Tenista 1 da dupla vencedora",
+        JOGADOR_INVALIDO,
+        validate_name,
+        "player1",
+        defaultMarkup
+    ),
+    (
+        "Insira a categoria do Tenista 1 da dupla vencedora "
+        "(C, B, A, S ou SS)",
+        "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
+        validate_cat,
+        "cat1",
+        catMarkup
+    ),
+    (
+        "Insira o nome do Tenista 2 da dupla vencedora",
+        JOGADOR_INVALIDO,
+        validate_name,
+        "player2",
+        defaultMarkup
+    ),
+    (
+        "Insira a categoria do Tenista 2 da dupla vencedora "
+        "(C, B, A, S ou SS)",
+        "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
+        validate_cat,
+        "cat2",
+        catMarkup
+    ),
+    (
+        "Insira o nome do Tenista 1 da dupla derrotada",
+        JOGADOR_INVALIDO,
+        validate_name,
+        "player3",
+        defaultMarkup
+    ),
+    (
+        "Insira a categoria do Tenista 1 da dupla derrotada "
+        "(C, B, A, S ou SS)",
+        "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
+        validate_cat,
+        "cat3",
+        catMarkup
+    ),
+    (
+        "Insira o nome do Tenista 2 da dupla derrotada",
+        JOGADOR_INVALIDO,
+        validate_name,
+        "player4",
+        defaultMarkup
+    ),
+    (
+        "Insira a categoria do Tenista 2 da dupla derrotada "
+        "(C, B, A, S ou SS)",
+        "Categoria Inválida. As categorias válidas são C, B, A, S ou SS",
+        validate_cat,
+        "cat4",
+        catMarkup
+    ),
+    (
+        "Insira o placar do jogo, sempre em referência à dupla "
+        "vencedora e apenas números. Exemplos: 60, 63, 75. "
+        "Em caso de tiebreak, informe os "
+        "pontos do perdedor entre parênteses, exemplos: 76(4), 76(9)",
+        "Placar inválido",
+        validate_score,
+        "score",
+        defaultMarkup
+    ),
+    (
+        "Insira o nome do local onde ocorreu o jogo",
+        "Local inválido",
+        validate_local,
+        "local",
+        defaultMarkup
     )
 ]
 
@@ -312,8 +531,12 @@ def placar(message):
         )
         return
 
-    state[chat_id] = 0
-    bot.send_message(chat_id=chat_id, text=stateHandler[state[chat_id]][0])
+    state[chat_id] = -1
+    bot.send_message(
+        chat_id=chat_id,
+        text=initialHandler[0],
+        reply_markup=initialHandler[4]
+    )
 
     # bot.register_next_step_handler(message, process_player1)
 
@@ -341,49 +564,84 @@ def process_inputs(message):
     chat_id = message.chat.id
     text = message.text.strip()
 
-    if not state or chat_id not in state or state[chat_id] < 0:
+    if not state or chat_id not in state or state[chat_id] < -1:
         bot.send_message(chat_id=chat_id, text=WELCOME)
         return
 
     currentState = state[chat_id]
+    # print(f"current state = {currentState}")
 
     if chat_id not in user_info:
         user_info[chat_id] = {}
 
-    if currentState == len(stateHandler):
+    if currentState == -1:
+        if (initialHandler[2](text)):
+            user_info[chat_id][initialHandler[3]] = text
+        else:
+            bot.send_message(
+                chat_id=chat_id,
+                text=initialHandler[1],
+                reply_markup=initialHandler[4]
+            )
+            return
+
+    state_handler = stateHandler
+    if "tipo" in user_info[chat_id] and user_info[chat_id]["tipo"].upper() == "DUPLAS":
+        state_handler = stateHandlerDuplas
+
+    """if currentState == len(state_handler):
         bot.send_message(
             chat_id=chat_id,
             text="para começar uma nova geração de placar, "
             "use novamente o comando /placar"
         )
-        return
+        return """
 
-    if (stateHandler[currentState][2](text)):
-        user_info[chat_id][stateHandler[currentState][3]] = text
+    # print(f"start msg state={currentState}")
+    if (state_handler[currentState][2](text)):
+        # print("inserting")
+        # print(f"key {state_handler[currentState][3]}, text {text}")
+        user_info[chat_id][state_handler[currentState][3]] = text
         # print(user_info[chat_id])
+
+        if "tipo" in user_info[chat_id] and user_info[chat_id]["tipo"].upper() == "DUPLAS":
+            state_handler = stateHandlerDuplas
+        else:
+            state_handler = stateHandler
 
         state[chat_id] += 1
 
-        if currentState + 1 < len(stateHandler):
+        if currentState + 1 < len(state_handler):
             bot.send_message(
                 chat_id=chat_id,
-                text=stateHandler[currentState + 1][0]
+                text=state_handler[currentState + 1][0],
+                reply_markup=state_handler[currentState + 1][4]
             )
         else:
             bot.send_message(
                 chat_id=chat_id,
                 text="criando a imagem, pode demorar alguns segundos ..."
             )
-            create_image(chat_id)
+            if user_info[chat_id]["tipo"].upper() == "DUPLAS":
+                create_image_duplas(chat_id)
+            else:
+                create_image(chat_id)
+
             logger.info(
                 (f"state len: {len(state)}\n"
                  f"user_info len: {len(user_info)}")
             )
-            state[chat_id] = -1
+            
+            #state[chat_id] = -1
+            state.pop(chat_id)
             # bot.send_message(chat_id=chat_id, text="acabou")
 
     else:
-        bot.send_message(chat_id=chat_id, text=stateHandler[currentState][1])
+        bot.send_message(
+            chat_id=chat_id,
+            text=state_handler[currentState][1],
+            reply_markup=state_handler[currentState][4]
+        )
 
 
 logger.info("running...")
